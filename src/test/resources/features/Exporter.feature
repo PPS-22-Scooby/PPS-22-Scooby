8206ce42e1ad7e550a4f6d3cf12b8ee738ad208f
FreshMag <pescomagnani@gmail.com>
2024-06-10 17:07:32 +0200

    Setup of Cucumber feature and corresponding scala steps using datatables and Cucumber expressions

--- src/test/resources/features/Exporter.feature
+++ src/test/resources/features/Exporter.feature
@@ -1,6 +1,18 @@
-Feature: Exporter feature
+Feature: Exporter
 
-  Scenario: Empty page scenario
-    Given I have no content to export
+  Scenario: Empty result
+    Given I have an Exporter
+    And an empty result
     When I try to export it
-    Then it should return an empty list
+    Then it should return ''
+
+  Scenario: Result to csv
+    Given I have an Exporter with a csv strategy
+    And the following result
+      | tag      | amount |
+      | <a>      | 10     |
+      | <button> | 20     |
+    When I try to export it
+    Then it should return 'tag;amount\n<a>;10\n<button>;20'
+
+
--- src/test/scala/exporter/StepDefinitions.scala
+++ src/test/scala/exporter/StepDefinitions.scala
@@ -1,18 +1,32 @@
 package org.unibo.scooby
 package exporter
 
+import io.cucumber.datatable.DataTable
 import io.cucumber.scala.{EN, ScalaDsl}
 
 class StepDefinitions extends ScalaDsl with EN:
 
-  Given("""^I have no content to export""") { () =>
+
+  Given("""^I have an Exporter""") { () =>
     println("I have no content to export")
+  }
+
+  And("""^an empty result""") { () =>
+    println("I have an empty result")
+  }
+
+  Given("""^I have an Exporter with a csv strategy""") { () =>
+    println("I have an Csv exporter")
+  }
+
+  And("""^the following result""") { (result: DataTable) =>
+    println(result)
   }
 
   When("""^I try to export it""") { () =>
     println("Exporting it")
   }
-
-  Then("""^it should return an empty list""") { () =>
-    println("Returning an empty list")
-  }
+  
+  Then("""it should return {string}""") { (result: String) => {
+    println(result)
+  }}
