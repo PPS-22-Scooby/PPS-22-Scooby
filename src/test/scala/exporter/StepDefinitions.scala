594ee8e1cf29c7e41f2d6ef2741835e3c76adb67
FreshMag <pescomagnani@gmail.com>
2024-06-11 17:45:09 +0200

    Implemented steps for Exporter.feature: added a dummy class to act as Exporter to make the steps compile

--- src/test/scala/exporter/StepDefinitions.scala
+++ src/test/scala/exporter/StepDefinitions.scala
@@ -1,32 +1,47 @@
 package org.unibo.scooby
 package exporter
 
+import exporter.DummyExporter.{CsvStrategy, Exporter, ListStrategy}
+
 import io.cucumber.datatable.DataTable
 import io.cucumber.scala.{EN, ScalaDsl}
+import org.junit.Assert.assertEquals
 
 class StepDefinitions extends ScalaDsl with EN:
 
+  private var exporter: Exporter = new Exporter()
 
-  Given("""^I have an Exporter""") { () =>
-    println("I have no content to export")
+  private var results: List[DataTable] = List()
+
+  private var stringResult = ""
+
+  Given("""I have an Exporter with a {string} strategy""") { (format: String) =>
+    exporter = format match
+      case "list" => new Exporter() with ListStrategy()
+      case "csv" => new Exporter() with CsvStrategy()
+      case _ => new Exporter()
   }
 
-  And("""^an empty result""") { () =>
-    println("I have an empty result")
+  And("""the following result""") { (input: DataTable) =>
+    exporter = exporter <-- input
   }
 
-  Given("""^I have an Exporter with a csv strategy""") { () =>
-    println("I have an Csv exporter")
+  And("""the following result of many""") { (input: DataTable) =>
+    results = input :: results
   }
 
-  And("""^the following result""") { (result: DataTable) =>
-    println(result)
-  }
-
-  When("""^I try to export it""") { () =>
-    println("Exporting it")
+  When("""I try to export it""") { () =>
+    stringResult = exporter.exportAsString
   }
   
-  Then("""it should return {string}""") { (result: String) => {
-    println(result)
-  }}
+  Then("""it should return {string}""") { (result: String) =>
+    assertEquals(result, stringResult)
+  }
+
+  Then("""it should return first {string} and then {string}""") { (result1: String, result2: String) =>
+    exporter = exporter <-- results.tail.head
+    assertEquals(result1, exporter.exportAsString)
+    exporter = exporter <-- results.head
+    assertEquals(result2, exporter.exportAsString)
+  }
+
