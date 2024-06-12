1b944b370c7409e1b2684d27b9dd51b623b85e23
FreshMag <70514090+FreshMag@users.noreply.github.com>
2024-06-12 18:13:33 +0200

    Test/exporter (#3)
    Complete the PPS-16 task
    
    ---------
    
    Co-authored-by: FreshMag <pescomagnani@gmail.com>
    Co-authored-by: Giovanni Antonioni <giovanni.antonioni2@studio.unibo.it>

---
+++ src/test/scala/exporter/DummyExporter.scala
@@ -0,0 +1,43 @@
+package org.unibo.scooby
+package exporter
+
+import io.cucumber.datatable.DataTable
+
+import scala.annotation.targetName
+
+object DummyExporter:
+
+  class Exporter:
+    
+    @targetName("consumeResult")
+    infix def <--(result: DataTable): Exporter = consume(result)
+    
+    def consume(result: DataTable): Exporter = this
+    
+    def exportAsString: String = ""
+
+  trait ExportStrategy extends Exporter:
+
+    override def consume(result: DataTable): Exporter =
+      super.consume(result)
+      publish(result)
+
+    def publish(result: DataTable): Exporter
+
+  trait NothingStrategy extends ExportStrategy:
+    override def publish(result: DataTable): Exporter = this
+
+    override def exportAsString: String =
+      s"""
+      ${super.exportAsString}
+      ## NOTHING STRATEGY:
+
+      I export nothing
+
+      """.trim
+      
+  trait ListCountStrategy extends ExportStrategy:
+    override def publish(result: DataTable): Exporter = this
+  
+  trait CsvCountStrategy extends ExportStrategy:
+    override def publish(result: DataTable): Exporter = this
---
+++ src/test/scala/exporter/ExporterTest.scala
@@ -0,0 +1,8 @@
+package org.unibo.scooby
+package exporter
+
+import io.cucumber.junit.Cucumber
+import org.junit.runner.RunWith
+
+@RunWith(classOf[Cucumber])
+class ExporterTest
