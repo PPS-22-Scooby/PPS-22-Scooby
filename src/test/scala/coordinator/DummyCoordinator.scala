b2c38226f40939c33767037573220178f0842e44
Valerio Di Zio <diziovalerio@gmail.com>
2024-06-16 12:41:31 +0200

    test: initial test Coordinator implementation

--- src/test/scala/coordinator/DummyCoordinator.scala
+++ src/test/scala/coordinator/DummyCoordinator.scala
@@ -4,9 +4,9 @@ package coordinator
 import scala.util.Random
 
 object DummyCoordinator {
-  class Coordinator:
+  class Coordinator {
 
-    private var crawledPages: List[String] = List.empty
+    private var crawledPages: Set[String] = Set.empty
 
     /**
      * Checks whether the provided pages have already been crawled and updates the list of crawled pages.
@@ -14,14 +14,52 @@ object DummyCoordinator {
      * @param pages The list of pages to be verified.
      * @return A map that associates each page with a boolean value indicating whether the page has already been crawled.
      */
-    def checkPages(pages: List[String]): Map[String, Boolean] =
+    def checkPages(pages: List[String]): Map[String, Boolean] = {
       pages.map { page =>
-        val isCrawled = crawledPages.contains(page)
-        if (!isCrawled) {
-          crawledPages = crawledPages.appended(page)
+        val normalizedPage = normalizeURL(page)
+        val isCrawled = crawledPages.contains(normalizedPage)
+        if (isValidURL(page) && !isCrawled) {
+          crawledPages += normalizedPage
         }
         page -> isCrawled
       }.toMap
-      
-    def getCrawledPages: List[String] = crawledPages
+    }
+
+    /**
+     * Sets the list of already crawled pages.
+     *
+     * @param pages The list of pages to set as already crawled.
+     */
+    def setCrawledPages(pages: List[String]): Unit = {
+      crawledPages = pages.filter(isValidURL).map(normalizeURL).toSet
+    }
+
+    /**
+     * Gets the list of already crawled pages.
+     *
+     * @return The list of already crawled pages.
+     */
+    def getCrawledPages: List[String] = crawledPages.toList
+
+    /**
+     * Normalizes the URL by removing the protocol for comparison purposes but keeping the protocol in the stored set.
+     *
+     * @param url The URL to be normalized.
+     * @return The normalized URL.
+     */
+    private def normalizeURL(url: String): String = {
+      url.replaceFirst("^(http://|https://)", "")
+    }
+
+    /**
+     * Validates if the provided URL is valid.
+     *
+     * @param url The URL to be validated.
+     * @return True if the URL is valid, otherwise false.
+     */
+    private def isValidURL(url: String): Boolean = {
+      val regex = "^(http://|https://)[a-zA-Z0-9\\-\\.]+\\.[a-zA-Z]{2,}(\\S*)?$"
+      url.matches(regex)
+    }
+  }
 }
--- src/test/scala/coordinator/StepDefinitions.scala
+++ src/test/scala/coordinator/StepDefinitions.scala
@@ -1,38 +1,53 @@
+
 package org.unibo.scooby
 package coordinator
 
-import io.cucumber.scala.{EN, ScalaDsl}
-import org.junit.Assert._
 import coordinator.DummyCoordinator.Coordinator
 
-class StepDefinitions extends ScalaDsl with EN:
+import io.cucumber.scala.{EN, ScalaDsl}
+import org.junit.Assert.*
+
+class StepDefinitions extends ScalaDsl with EN {
 
   private val coordinator = Coordinator()
   private var pages: List[String] = List.empty
   private var crawablePages: Map[String, Boolean] = Map.empty
   private var checkResult: Option[Boolean] = None
 
-  Given("""I have a list of (.*)$""") { (pages: String) =>
-    this.pages = pages.split(",").toList
+  Given("""I have a list of already crawled pages (.*)$""") { (crawledPages: String) =>
+    val pages = crawledPages.split(",").map(_.trim).toList
+    coordinator.setCrawledPages(pages)
   }
 
-  When("""Crawler requests the ability to scrape pages""") { () =>
-    this.crawablePages = coordinator.checkPages(this.pages)
-  }
-
-  Then("""They should return a map of boolean""") { () =>
-    assertTrue(this.crawablePages.nonEmpty)
-  }
-
-  Given("""I have this (.*) are already crawled$""") { (pages: String) =>
-    val crawledPages = pages.replaceAll("\\s", "").split(",").toList
-    coordinator.checkPages(crawledPages)
+  Given("""I have an empty list of already crawled pages""") { () =>
+    coordinator.setCrawledPages(List.empty)
   }
 
   When("""I check if (.*) is already crawled$""") { (page: String) =>
     this.checkResult = coordinator.checkPages(List(page)).values.headOption
   }
 
-  Then("""The result should be (true|false)$""") { (expectedResult: Boolean) =>
-    assertEquals(expectedResult, this.checkResult.getOrElse(false))
+  When("""I add (.*) to the crawled list$""") { (newPages: String) =>
+    val pages = newPages.split(",").map(_.trim).toList
+    coordinator.checkPages(pages)
   }
+
+  Then("""The result should be (true|false)$""") { (expectedResult: Boolean) =>
+    assertEquals(expectedResult, this.checkResult.getOrElse(true))
+  }
+
+  Then("""The updated crawled list should be (.*)$""") { (updatedList: String) =>
+    val expectedList = updatedList.split(",").map(_.trim).toList
+    assertEquals(expectedList, coordinator.getCrawledPages)
+  }
+
+  Then("""Only valid URLs should be added to the list, resulting in (.*)$""") { (updatedList: String) =>
+    val expectedList = updatedList.split(",").map(_.trim).toList
+    assertEquals(expectedList, coordinator.getCrawledPages)
+  }
+
+  Then("""The updated crawled list should not contain duplicates and be (.*)$""") { (updatedList: String) =>
+    val expectedList = updatedList.split(",").map(_.trim).toList
+    assertEquals(expectedList, coordinator.getCrawledPages.distinct)
+  }
+}
