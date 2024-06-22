d93fbc02dba14bbe06a0428a4e1be62deee73b3f
FreshMag <pescomagnani@gmail.com>
2024-06-22 11:47:18 +0200

    test: PPS-28 implemented acceptance tests for HttpLibraryBasicUsage.feature

--- src/test/scala/utility/http/BasicUsageStepDefinitions.scala
+++ src/test/scala/utility/http/BasicUsageStepDefinitions.scala
@@ -2,34 +2,36 @@ package org.unibo.scooby
 package utility.http
 
 import io.cucumber.scala.{EN, ScalaDsl}
+import org.unibo.scooby.utility.http.Request.RequestBuilder
+import org.scalatest.Assertions.*
+
 
 class BasicUsageStepDefinitions extends ScalaDsl with EN:
 
-  Given("""a simple {string} request"""): (requestType: String) =>
-    // Write code here that turns the phrase above into concrete actions
-    throw new io.cucumber.scala.PendingException()
+  var request: RequestBuilder = Request.builder
+  var response: Response = Response.empty
+  given httpClient: HttpClient = HttpClient()
 
-  Given("""a URL {string}"""): (string: String) =>
-    // Write code here that turns the phrase above into concrete actions
-    throw new io.cucumber.scala.PendingException()
+  Given("""a simple {string} request"""): (requestType: String) =>
+    request = request.method(HttpMethod.valueOf(requestType))
+
+  Given("""a URL {string}"""): (url: String) =>
+    request = request.at(url)
 
 
   When("""i make the HTTP call"""): () =>
-    // Write code here that turns the phrase above into concrete actions
-    throw new io.cucumber.scala.PendingException()
+    response = request.send
 
 
   Then("""the returned content should be not empty"""): () =>
-    // Write code here that turns the phrase above into concrete actions
-    throw new io.cucumber.scala.PendingException()
+    assert(response.body.nonEmpty)
 
   Then("""it should return an error"""): () =>
-    // Write code here that turns the phrase above into concrete actions
-    throw new io.cucumber.scala.PendingException()
+    assert(response.status === HttpStatus.NOT_FOUND)
 
 
   Then("""the status code should be {int} and the header Content-Type {string}"""):
     (statusCode: Int, contentType: String) =>
-    // Write code here that turns the phrase above into concrete actions
-    throw new io.cucumber.scala.PendingException()
+    assert(response.status.code === statusCode)
+    assert(response.headers("Content-Type") === contentType)
 
