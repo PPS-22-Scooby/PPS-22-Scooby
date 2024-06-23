69906fb1239ea22d0f9ad758eef3b107b40f52d3
Giovanni Antonioni <giovanni.antonioni2@studio.unibo.it>
2024-06-23 23:56:40 +0200

    doc: PPS-29 change documentation for Rule ADT

--- src/main/scala/utility/rule/Rule.scala
+++ src/main/scala/utility/rule/Rule.scala
@@ -9,22 +9,52 @@ package utility.rule
  */
 enum Rule[A, B]:
 
+  /** Represents a base rule that wraps a function from `A` to `B`. */
   case Base(f: A => B)
+
+  /** Represents a policy rule that wraps a predicate function from `A` to `Boolean`. */
   case Policy(f: A => Boolean)
-  case Invalid()
   
+  /** Represents an invalid rule. */
+  case Invalid()
+
+  /**
+   * Combines this rule with another rule using logical AND.
+   *
+   * @param other the other rule
+   * @return a new rule that represents the logical AND of this rule and the other rule
+   */
   def and(other: Rule[A, Boolean]): Rule[A, Boolean] = (this, other) match
     case (Policy(p1), Policy(p2)) => Policy((a: A) => p1(a) && p2(a))
     case _ => Invalid()
 
+  /**
+   * Combines this rule with another rule using logical OR.
+   *
+   * @param other the other rule
+   * @return a new rule that represents the logical OR of this rule and the other rule
+   */
   def or(other: Rule[A, Boolean]): Rule[A, Boolean] = (this, other) match
     case (Policy(p1), Policy(p2)) => Policy((a: A) => p1(a) || p2(a))
     case _ => Invalid()
 
+  /**
+   * Composes this rule with another rule.
+   *
+   * @param other the other rule
+   * @tparam C the output type of the other rule
+   * @return a new rule that represents the composition of this rule and the other rule
+   */
   def >>[C](other: Rule[B, C]): Rule[A, C] = (this, other) match
     case (Base(f), Base(g)) => Base(f andThen g)
     case _ => Invalid()
 
+  /**
+   * Executes this rule on a value.
+   *
+   * @param value the value to execute the rule on
+   * @return the result of the rule execution
+   */
   def executeOn(value: A): Option[?] = this match
     case Base(f) => Some(f(value))
     case Policy(p) => Some(p(value))
@@ -41,6 +71,13 @@ object Rule:
    */
   def apply[A, B](f: A => B): Rule[A, B] = Base(f)
 
+  /**
+   * Creates a new `Policy` from a predicate function.
+   *
+   * @param f the predicate function to wrap in a `Policy`
+   * @tparam A the input type of the rule
+   * @return a new `Policy` that wraps the predicate function `f`
+   */
   def policy[A](f: A => Boolean): Rule[A, Boolean] = Policy(f)
 
   /**
