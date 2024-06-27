4cf8ab03f0643ba401a0ca1b5ee2a279c395e1df
FreshMag <70514090+FreshMag@users.noreply.github.com>
2024-06-27 17:44:45 +0200

    PPS-30 PPS-39 Tuned and Integrated http library (#9)
    * integration: PPS-30 switched from Try to Either return type inside the send method of RequestBuilder
    
    * integration: PPS-30 switched URL parsing result type to Either[String, URL] instead of Try[URL]
    
    * integration: PPS-30 integrated utility.http.URL class into utility.document.Document class (migrated from java.net.URL)
    
    * integration: PPS-30 fixed documentation and completely removed the Try class usages from utility.http classes
    
    ---------
    
    Co-authored-by: FreshMag <pescomagnani@gmail.com>

--- src/test/scala/utility/document/DocumentTest.scala
+++ src/test/scala/utility/document/DocumentTest.scala
@@ -1,10 +1,11 @@
 package org.unibo.scooby
 package utility.document
 
+import utility.http.URL
+
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should
 
-import java.net.URL
 
 class DocumentTest extends AnyFlatSpec with should.Matchers:
 
@@ -15,9 +16,13 @@ class DocumentTest extends AnyFlatSpec with should.Matchers:
                     "</head>" +
                   "<body></body>" +
                 "</html>"
-      val URL = new URL("http://www.example.com")
-      val document = Document(html, URL)
-      document.content should be (html)
-      document.url should be (URL)
+      val urlEither = URL("http://www.example.com")
+      assert(urlEither.isRight)
+      urlEither.fold(_ => fail("Illegal URL"), url =>
+        val document = Document(html, url)
+        document.content should be (html)
+        document.url should be (url)
+      )
+      
 
 
--- src/test/scala/utility/http/BasicUsageStepDefinitions.scala
+++ src/test/scala/utility/http/BasicUsageStepDefinitions.scala
@@ -7,13 +7,11 @@ import utility.http.Request.RequestBuilder
 import io.cucumber.scala.{EN, ScalaDsl}
 import org.scalatest.Assertions.*
 
-import scala.util.Try
-
 
 class BasicUsageStepDefinitions extends ScalaDsl with EN:
 
   var request: RequestBuilder = Request.builder
-  var response: Try[Response] = Try { Response.empty }
+  var response: Either[String, Response] = Left("Empty response")
   given httpClient: SimpleHttpClient = SimpleHttpClient()
 
   Given("""a simple {string} request"""): (requestType: String) =>
@@ -28,14 +26,15 @@ class BasicUsageStepDefinitions extends ScalaDsl with EN:
 
 
   Then("""the returned content should be not empty"""): () =>
-    assert(response.get.body.nonEmpty)
+    assert(response.isRight)
+    assert(response.fold(_ => false, _.body.nonEmpty))
 
   Then("""it should return an error"""): () =>
-    assert(response.isFailure)
+    assert(response.isLeft)
 
 
   Then("""the status code should be {int} and the header content-type {string}"""):
     (statusCode: Int, contentType: String) =>
-    assert(response.get.status.code === statusCode)
-    assert(response.get.headers("content-type") === contentType)
+    assert(response.fold(_ => false, _.status.code === statusCode))
+    assert(response.fold(_ => false, _.headers("content-type") === contentType))
 
