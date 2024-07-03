5c2379f645746bfd9c413b71e4db19900ca3c8dd
FreshMag <pescomagnani@gmail.com>
2024-07-03 22:06:46 +0200

    feature: PPS-41 restructured HTTP request making DSL, now working

--- src/main/scala/utility/http/api/Calls.scala
+++ src/main/scala/utility/http/api/Calls.scala
@@ -1,6 +1,8 @@
 package org.unibo.scooby
 package utility.http.api
 
+import utility.http.Header
+import utility.http.HttpMethod.{GET, POST}
 import utility.http.Request.RequestBuilder
 
 /**
@@ -8,30 +10,36 @@ import utility.http.Request.RequestBuilder
  */
 object Calls:
   import utility.http.{Client, Deserializer, HttpMethod, Request}
-  import Context.*
 
-  private def fetch[T, R: Client](url: String)(using
-    deserializer: Deserializer[R, T]
-  )(method: HttpMethod)(init: RequestContext ?=> RequestBuilder): Either[String, T] =
-    val builder = Request.builder.at(url).method(method)
-    given context: RequestContext = new RequestContext(builder)
-    init
-    builder.build.flatMap {
-      _.send[R](summon[Client[R]]).map(deserializer.deserialize)
-    }.left.map(identity)
+  private type RequestContext = RequestBuilderMutable ?=> Unit
 
-  private def directBuild(using context: RequestContext): RequestBuilder = context.builder
+  private def buildUntouched(using RequestBuilderMutable): RequestBuilder = summon[RequestBuilderMutable].builder
 
-  def GET[T, R: Client](url: String, init: RequestContext ?=> RequestBuilder = directBuild)(using
-    deserializer: Deserializer[R, T]
-  ): Either[String, T] =
-    fetch(url)(HttpMethod.GET)(init)
+  def GET[T, R: Client](url: String): PartialCall[T, R] =
+    new PartialCall[T, R](url, HttpMethod.GET)
 
-  def POST[T, R: Client](url: String, init: RequestContext ?=> RequestBuilder = directBuild)(using
-    deserializer: Deserializer[R, T]
-  ): Either[String, T] =
-    fetch(url)(HttpMethod.POST)(init)
+  def POST[T, R: Client](url: String): PartialCall[T, R] =
+    new PartialCall[T, R](url, HttpMethod.POST)
 
-  private object Context:
+  def Body(init: => String)(using mutableBuilder: RequestBuilderMutable): Unit =
+    val mutableBuilder = summon[RequestBuilderMutable]
+    mutableBuilder.builder = mutableBuilder.builder.body(init)
 
-    class RequestContext(var builder: RequestBuilder)
+  def Headers(init: => Seq[Header])(using mutableBuilder: RequestBuilderMutable): Unit =
+    mutableBuilder.builder = mutableBuilder.builder.headers(init*)
+
+  class PartialCall[T, R: Client](val url: String, val method: HttpMethod):
+    infix def sending(init: RequestContext)(using
+      deserializer: Deserializer[R, T]
+    ): Either[String, T] =
+      given mutableBuilder: RequestBuilderMutable = RequestBuilderMutable(Request.builder.at(url).method(method))
+      init
+      mutableBuilder.builder.build.flatMap {
+        _.send[R](summon[Client[R]]).map(deserializer.deserialize)
+      }.left.map(identity)
+
+  given call2send[T, R: Client](using Deserializer[R, T]): Conversion[PartialCall[T, R], Either[String, T]] with
+    override def apply(partialCall: PartialCall[T, R]): Either[String, T] =
+      partialCall sending buildUntouched
+
+  sealed case class RequestBuilderMutable(var builder: RequestBuilder)
