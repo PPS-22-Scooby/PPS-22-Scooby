d826823682c0b9b0b945d554bae8d3612d9227f7
FreshMag <pescomagnani@gmail.com>
2024-07-04 18:20:06 +0200

    feature: PPS-41 added support for HTTP client configuration: added class ClientConfiguration as container of the client's settings

--- src/main/scala/utility/http/Client.scala
+++ src/main/scala/utility/http/Client.scala
@@ -1,10 +1,14 @@
 package org.unibo.scooby
 package utility.http
 
+import utility.http.Configuration.ClientConfiguration
+
+import scala.concurrent.duration.Duration
+
 /**
  * Empty trait that represents an HTTP client. To be useful, you need to mix-in [[Backend]] Traits
  */
-trait HttpClient
+trait HttpClient(configuration: ClientConfiguration)
 
 object Clients:
   import utility.http.Backends.SttpBackend
@@ -12,6 +16,27 @@ object Clients:
   /**
    * Simple HTTP client used for synchronous HTTP calls with the sttp library as backend
    */
-  class SimpleHttpClient extends HttpClient with SttpBackend
+  class SimpleHttpClient(configuration: ClientConfiguration = Configuration.default)
+      extends HttpClient(configuration) with SttpBackend
 
 type Client[R] = HttpClient & Backend[R]
+
+object Configuration:
+  private type ConfigurationEntry = (ConfigurationEntryType, Any)
+
+  enum ConfigurationEntryType(val valueType: Class[?]):
+    case NETWORK_TIMEOUT extends ConfigurationEntryType(classOf[Duration])
+
+  case class ClientConfiguration(configs: ConfigurationEntry*)
+
+  object ClientConfiguration:
+    def apply(configs: ConfigurationEntry*): Either[HttpError, ClientConfiguration] =
+      configs.find {
+        case (entryType: ConfigurationEntryType, value: Any) => value.getClass != entryType
+      } match
+        case Some((entryType: ConfigurationEntryType, entryValue: Any)) =>
+          Left((s"$entryValue doesn't satisfy the type of configuration entry: $entryType which " +
+            s"is ${entryType.valueType}").asHttpError)
+        case None => Right(new ClientConfiguration(configs*))
+
+  def default: ClientConfiguration = new ClientConfiguration()
