f822b84322e722fb5053054c8bc6855fd204866d
Valerio Di Zio <diziovalerio@gmail.com>
2024-07-05 16:30:30 +0200

    feat: PPS-49 integration of robots.txt in the crawler

--- src/main/scala/core/coordinator/Robots.scala
+++ src/main/scala/core/coordinator/Robots.scala
@@ -4,8 +4,21 @@ package core.coordinator
 import utility.http.Clients.SimpleHttpClient
 import utility.http.{Request, Response, URL}
 
+/**
+ * The `Robots` object provides functionalities to fetch and parse the robots.txt file from a given website. It also
+ * includes a method to check if a given URL is allowed to be visited according to the rules specified in the robots.txt
+ * file.
+ */
 object Robots {
 
+  /**
+   * Fetches the robots.txt file from the specified site URL.
+   *
+   * @param siteUrl
+   *   The URL of the site from which to fetch the robots.txt file.
+   * @return
+   *   The content of the robots.txt file as a String. Returns an error message if fetching fails.
+   */
   def fetchRobotsTxt(siteUrl: String): String = {
     val httpClient: SimpleHttpClient = SimpleHttpClient()
     val parsedUrl = URL(siteUrl).getOrElse(URL.empty)
@@ -25,25 +38,50 @@ object Robots {
             }
   }
 
-  def parseRobotsTxt(robotsTxt: String): List[String] = {
-    val lines = robotsTxt.split("\n")
-    var userAgent: Option[String] = None
-    var disallowRules: List[String] = List()
+  /**
+   * Parses the content of a robots.txt file and extracts the disallow rules.
+   *
+   * @param robotsTxt
+   *   The content of the robots.txt file as a String.
+   * @return
+   *   A List of disallowed paths for the user-agent "*".
+   */
+  def parseRobotsTxt(robotsTxt: String): Set[String] = {
+    val lines = robotsTxt.split("\n").toList
+    val initialState = (Option.empty[String], List.empty[String])
 
-    for (line <- lines) {
-      val trimmedLine = line.trim
-      if (trimmedLine.startsWith("User-agent:")) {
-        userAgent = Some(trimmedLine.split(":", 2)(1).trim)
-      } else if (trimmedLine.startsWith("Disallow:") && userAgent.contains("*")) {
-        val path = trimmedLine.split(":", 2)(1).trim
-        if (path.nonEmpty) disallowRules ::= path
-      }
+    val (_, disallowRules) = lines.foldLeft(initialState) {
+      case ((userAgent, disallowRules), line) =>
+        val trimmedLine = line.trim
+        if (trimmedLine.startsWith("#")) {
+          // Skip comments
+          (userAgent, disallowRules)
+        } else {
+          trimmedLine.split(":", 2) match {
+            case Array("User-agent", ua) =>
+              (Some(ua.trim), disallowRules)
+            case Array("Disallow", path) if userAgent.contains("*") && path.trim.nonEmpty =>
+              (userAgent, path.trim :: disallowRules)
+            case _ =>
+              (userAgent, disallowRules)
+          }
+        }
     }
 
-    disallowRules
+    disallowRules.toSet
   }
 
-  def canVisit(url: String, disallowRules: List[String]): Boolean = {
+  /**
+   * Checks if a given URL is allowed to be visited according to the disallow rules.
+   *
+   * @param url
+   *   The URL to check.
+   * @param disallowRules
+   *   A List of disallowed paths.
+   * @return
+   *   `true` if the URL is allowed to be visited, `false` otherwise.
+   */
+  def canVisit(url: String, disallowRules: Set[String]): Boolean = {
     val parsedUrl = URL(url).getOrElse(URL.empty).toString
     !disallowRules.exists(rule => parsedUrl.contains(rule))
   }
