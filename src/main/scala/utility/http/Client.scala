333f1ec45b5a2e101fd6ccab5e742548afd5312c
FreshMag <pescomagnani@gmail.com>
2024-07-05 17:55:05 +0200

    doc: PPS-41 added scala doc to Client.scala

--- src/main/scala/utility/http/Client.scala
+++ src/main/scala/utility/http/Client.scala
@@ -34,6 +34,9 @@ trait Backend[R] extends HttpClient:
  */
 trait HttpClient(val configuration: ClientConfiguration)
 
+/**
+ * Collection of useful clients types, using different backends.
+ */
 object Clients:
   import utility.http.backends.SttpSyncBackend
 
@@ -43,23 +46,65 @@ object Clients:
   class SimpleHttpClient(configuration: ClientConfiguration = Configuration.default)
       extends HttpClient(configuration) with SttpSyncBackend
 
+/**
+ * Type alias to represent a [[Client]] with a [[Backend]]
+ * @tparam R type of the [[Backend]]
+ */
 type Client[R] = HttpClient & Backend[R]
 
+/**
+ * Object that contains configuration classes for [[Client]]
+ */
 object Configuration:
   import scala.concurrent.duration.DurationInt
 
+  /**
+   * Enum that represents various types of settings applicable to [[ClientConfiguration]]
+   * @param value value of this setting
+   * @tparam T type of the value of this setting
+   */
   enum Property[T](val value: T):
+    /**
+     * Type of property that represents the network timeout. Works by setting a [[FiniteDuration]]
+     */
     case NetworkTimeout(override val value: FiniteDuration) extends Property[FiniteDuration](value)
+    /**
+     * Type of property that represents the maximum amount of requests that can be made within this client
+     */
     case MaxRequests(override val value: Int) extends Property[Int](value)
 
+  /**
+   * Class representing the configuration of a [[Client]]. It can contain various types of [[Property]].
+   * @param properties [[Seq]] of [[Property]] that globally represent the collection of settings for this [[Client]].
+   *                   One type of [[Property]] is meant to be added only once inside this collection.
+   */
   class ClientConfiguration(properties: Seq[Property[?]]):
+    /**
+     * Returns the value of the [[Property]] requested.
+     * @param tag [[ClassTag]] to prevent type erasure for type [[T]], representing the [[Property]] requested
+     * @tparam T type of the [[Property]] requested
+     * @tparam R type of the value inside the [[Property]] requested
+     * @return the value of type [[R]] inside the [[Property]]
+     */
     def property[T <: Property[R], R](implicit tag: ClassTag[T]): Option[R] =
       properties.collectFirst {
         case p if tag.runtimeClass.isInstance(p) =>
           p.asInstanceOf[T].value
       }
 
+  /**
+   * Companion object for the [[ClientConfiguration]] that contains its `apply` method
+   */
   object ClientConfiguration:
+    /**
+     * Useful instantiation method for [[ClientConfiguration]] providing a variable-length argument
+     * @param properties variable-length argument for [[Property]]
+     * @return a [[ClientConfiguration]] with those [[Property]]
+     */
     def apply(properties: Property[?]*): ClientConfiguration = new ClientConfiguration(properties.toSeq)
 
+  /**
+   * Configuration used by default by the [[Client]]s
+   * @return the default [[ClientConfiguration]]
+   */
   def default: ClientConfiguration = ClientConfiguration(NetworkTimeout(5.seconds))
