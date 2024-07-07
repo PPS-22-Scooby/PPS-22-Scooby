3f1e8bb1b7423d8b73f0f6bb8166d12f651f6fe6
Luca Rubboli <luca.rubboli2@studio.unibo.it>
2024-07-07 23:31:08 +0200

    PPS-46 chore: updated doc, re-designed cucumber tests for ScraperActor

--- src/main/scala/core/scraper/ScraperActor.scala
+++ src/main/scala/core/scraper/ScraperActor.scala
@@ -6,8 +6,19 @@ import utility.message.CommonMessages.{CommonMessages, onPaused}
 
 import akka.actor.{Actor, ActorSystem, Props, Stash}
 
+/**
+ * A type representing a function that extract an {@link Iterable} used to build {@link Result} from a {@link Document}
+ * @tparam D a type which is a subtype of {@link Document} type.
+ * @tparam T a generic type which represents the expected {@link Result} type.
+ */
 type IterableFromDoc[D <: Document, T] = D => Iterable[T]
 
+/**
+ * Class representing Scraper actor.
+ * @param scrapeRule the scraping rule the actor uses.
+ * @tparam D the type representing the {@link Document} to which apply the rule.
+ * @tparam T type representing the {@link Result} type.
+ */
 class ScraperActor[D <: Document, T](val scrapeRule: D => Iterable[T]) extends Actor with Stash:
 
   import ScraperActor.*
@@ -18,6 +29,10 @@ class ScraperActor[D <: Document, T](val scrapeRule: D => Iterable[T]) extends A
   private def applyRule(argument: D): Iterable[T] =
     scrapeRule(argument)
 
+  /**
+   * Function representing actor's behavior.
+   * @return
+   */
   def receive: Receive =
     case CommonMessages.Pause =>
       onPause()
@@ -26,15 +41,28 @@ class ScraperActor[D <: Document, T](val scrapeRule: D => Iterable[T]) extends A
       val result: Result[T] = Result(applyRule(doc))
       sender() ! Messages.SendPartialResult(result) // TODO: send to explorer when ready
 
-
+/**
+ * Companion object for the Scraper actor.
+ */
 object ScraperActor:
 
   def props[D <: Document, T](scrapeRule: D => Iterable[T]): Props = Props(new ScraperActor(scrapeRule))
 
+  /**
+   * Enum representing all {@link ScraperActor} messages.
+   */
   enum Messages:
     case Scrape[D <: Document](doc: D)
     case SendPartialResult[T](result: Result[T])
 
+  /**
+   * Utility for scraper's rules based on selectBy attribute, given selectors specified.
+   * Admissible values are id, tag, class and css.
+   *
+   * @param selectors a {@link Seq} of selectors used in scraper rule.
+   * @param selectBy a selector to specify the rule.
+   * @return the selected rule with specified selectors.
+   */
   def scraperRule(selectors: Seq[String], selectBy: String): IterableFromDoc[ScrapeDocument, String] = (scraper: ScrapeDocument) =>
     selectBy match
       case "id" =>
@@ -46,21 +74,58 @@ object ScraperActor:
       case "css" =>
         selectors.flatMap(scraper.select(_)).map(_.text)
 
+  /**
+   * A scraper rule based on elements' ids given.
+   * @param ids a {@link Seq} of ids used in the rule.
+   * @return the rule based on elements' ids.
+   */
   def scraperIdSelectorRule(ids: Seq[String]): IterableFromDoc[ScrapeDocument, String] = (scraper: ScrapeDocument) =>
     ids.map(scraper.getElementById).map(_.text)
 
+  /**
+   * A scraper rule based on elements' tags given.
+   *
+   * @param tags a {@link Seq} of tags used in the rule.
+   * @return the rule based on elements' tags.
+   */
   def scraperTagSelectorRule(tags: Seq[String]): IterableFromDoc[ScrapeDocument, String] = (scraper: ScrapeDocument) =>
     tags.flatMap(scraper.getElementByTag).map(_.text)
 
-  def scraperClassSelectorRule(classNames: Seq[String]): IterableFromDoc[ScrapeDocument, String] = (scraper: ScrapeDocument) =>
-    classNames.flatMap(scraper.getElementByClass).map(_.text)
+  /**
+   * A scraper rule based on elements' classes given.
+   *
+   * @param classesNames a {@link Seq} of classes used in the rule.
+   * @return the rule based on elements' classes.
+   */
+  def scraperClassSelectorRule(classesNames: Seq[String]): IterableFromDoc[ScrapeDocument, String] = (scraper: ScrapeDocument) =>
+    classesNames.flatMap(scraper.getElementByClass).map(_.text)
 
+  /**
+   * A scraper rule based on css selectors given.
+   *
+   * @param selectors a {@link Seq} of selectors used in the rule.
+   * @return the rule based on css selectors.
+   */
   def scraperCSSSelectorsRule(selectors: Seq[String]): IterableFromDoc[ScrapeDocument, String] = (scraper: ScrapeDocument) =>
     selectors.flatMap(scraper.select(_)).map(_.text)
 
+  /**
+   * A scraper rule based on regular expressions given.
+   *
+   * @param regex a {@link Seq} of regex used in the rule.
+   * @return the rule based on regex.
+   */
   def regexSelectorsRule(regex: Seq[String]): IterableFromDoc[RegExpExplorer, String] = (regExScraper: RegExpExplorer) =>
     regex.flatMap(regExScraper.find)
 
+  /**
+   * A scraper rule which concatenates a rule with selectors and one with regex.
+   *
+   * @param selectors the {@link Seq} of selectors used in the first rule.
+   * @param selectBy a selector to specify the rule.
+   * @param regex the {@link Seq} of regex to apply in the second rule.
+   * @return a rule that can be applied to {@link ScrapeDocument} to obtain an {@link Iterable} of String as a result of the concatenated rules.
+   */
   def scrapeSelectThenRegex(selectors: Seq[String], selectBy: String)(regex: Seq[String]): IterableFromDoc[ScrapeDocument, String] = (scraper: ScrapeDocument) =>
     val doc = new Document(
       scraperRule(selectors, selectBy)(scraper).reduceOption(_ concat _).getOrElse(""),
--- src/main/scala/utility/message/CommonMessages.scala
+++ src/main/scala/utility/message/CommonMessages.scala
@@ -4,12 +4,23 @@ package utility.message
 import akka.actor.Actor.Receive
 import akka.actor.{Actor, Props, Stash}
 
+/**
+ * Common messages between actors.
+ */
 object CommonMessages:
 
+  /**
+   * Enum representing all common messages.
+   */
   enum CommonMessages:
     case Pause
     case Resume
 
+  /**
+   * Regulates pause {@link Behavior} of the system actors.
+   * @tparam T the {@link Actor} to which apply the paused behavior.
+   * @return a new {@link Receive} function.
+   */
   def onPaused[T <: Actor with Stash]: (T, Receive) => Receive = (actor, activeReceive) =>
     case CommonMessages.Resume =>
       actor.unstashAll()
