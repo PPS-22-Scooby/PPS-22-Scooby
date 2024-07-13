87834da64cb855c3ed21c59b3696a63cf6de38ac
FreshMag <pescomagnani@gmail.com>
2024-07-13 12:00:43 +0200

    doc: PPS-59 added documentation inside Crawler

--- src/main/scala/core/crawler/Crawler.scala
+++ src/main/scala/core/crawler/Crawler.scala
@@ -4,16 +4,16 @@ package core.crawler
 import core.coordinator.CoordinatorCommand
 import core.crawler.Crawler.getCrawlerName
 import core.exporter.ExporterCommands
-import core.scraper.{Scraper, ScraperCommands}
 import core.scraper.ScraperPolicies.ScraperPolicy
+import core.scraper.{Scraper, ScraperCommands}
 import utility.document.{CrawlDocument, Document, ScrapeDocument}
 import utility.http.Clients.SimpleHttpClient
+import utility.http.Configuration.ClientConfiguration
 import utility.http.api.Calls.GET
 import utility.http.{Configuration, HttpError, HttpErrorType, URL}
 
 import akka.actor.typed.scaladsl.{ActorContext, Behaviors, StashBuffer}
-import akka.actor.typed.{ActorRef, Behavior, Terminated}
-import org.unibo.scooby.utility.http.Configuration.ClientConfiguration
+import akka.actor.typed.{ActorRef, Behavior}
 
 import scala.language.postfixOps
 
@@ -34,6 +34,9 @@ enum CrawlerCommand:
    */
   case CrawlerCoordinatorResponse(result: Iterator[String])
 
+  /**
+   * Command for when a child of this Crawler gets terminated
+   */
   case ChildTerminated()
 
 object Crawler:
@@ -44,6 +47,12 @@ object Crawler:
    *   the ActorRef of the coordinator to communicate with
    * @param exporter
    *   the ActorRef of the exporter to communicate with
+   * @param scrapeRule scraping rule for the [[Scraper]]s spawned by this Crawler
+   * @param explorationPolicy policy that specifies what links to explore inside the [[CrawlDocument]]
+   * @param maxDepth max recursion depth for crawlers (maximum depth of the tree of crawlers)
+   * @param networkConfiguration network configuration for the HTTP client
+   * @tparam D type of [[Document]] managed by the scraper (TODO replace with [[ScrapeDocument]]))
+   * @tparam T type of [[Result]]s that get exported
    * @return
    *   the behavior of the Crawler actor
    */
@@ -60,20 +69,32 @@ object Crawler:
         context => new Crawler[D, T](context, coordinator, exporter, scrapeRule, explorationPolicy,
           maxDepth, networkConfiguration, buffer).idle()
 
+  /**
+   * Obtains a simple identifier for a crawler given its URL
+   * @param url url of the given Crawler
+   * @return a simple name of the Crawler
+   */
   def getCrawlerName(url: URL): String =
     "[^a-zA-Z0-9\\-_.*$+:@&=,!~';]".r.replaceAllIn(url.withoutProtocol.filter(_ <= 0x7f), ".")
 
 
+/**
+ * Type that specifies a function that, given a [[CrawlDocument]], returns the links that need to be explored inside it
+ */
 type ExplorationPolicy = CrawlDocument => Iterable[URL]
 /**
  * Class representing a Crawler actor.
  *
- * @param context
- *   the ActorContext of the Crawler actor
  * @param coordinator
  *   the ActorRef of the coordinator to communicate with
  * @param exporter
- * *   the ActorRef of the exporter to communicate with
+ *   the ActorRef of the exporter to communicate with
+ * @param scrapeRule scraping rule for the [[Scraper]]s spawned by this Crawler
+ * @param explorationPolicy policy that specifies what links to explore inside the [[CrawlDocument]]
+ * @param maxDepth max recursion depth for crawlers (maximum depth of the tree of crawlers)
+ * @param networkConfiguration network configuration for the HTTP client
+ * @tparam D type of [[Document]] managed by the scraper (TODO replace with [[ScrapeDocument]]))
+ * @tparam T type of [[Result]]s that get exported
  */
 class Crawler[D <: Document, T](context: ActorContext[CrawlerCommand],
                                 coordinator: ActorRef[CoordinatorCommand],
@@ -86,6 +107,10 @@ class Crawler[D <: Document, T](context: ActorContext[CrawlerCommand],
                                ):
   import CrawlerCommand.*
 
+  /**
+   * Client used for HTTP requests
+   * @return the client used
+   */
   given httpClient: SimpleHttpClient = SimpleHttpClient(networkConfiguration)
 
   /**
@@ -95,9 +120,18 @@ class Crawler[D <: Document, T](context: ActorContext[CrawlerCommand],
    *   the behavior of the Crawler actor
    */
   def idle(): Behavior[CrawlerCommand] =
-
+    /**
+     * Sub-behavior of the crawler. It fetches the URL and starts crawling.
+     * @param url seed URL
+     * @return the crawling behavior
+     */
     def crawl(url: URL): Behavior[CrawlerCommand] =
 
+      /**
+       * Internal function used to handle HTTP errors
+       * @param e error to handle
+       * @return the behavior in case an error occurred
+       */
       def handleError(e: HttpError): Behavior[CrawlerCommand] =
         e match
           case HttpError(_, HttpErrorType.NETWORK) | HttpError(_, HttpErrorType.GENERIC) =>
@@ -107,11 +141,19 @@ class Crawler[D <: Document, T](context: ActorContext[CrawlerCommand],
 
         Behaviors.stopped
 
+      /**
+       * Internal function to handle the scraping (Scraper spawning and watching)
+       * @param document document obtained by fetching the URL
+       */
       def scrape(document: CrawlDocument): Unit =
         val scraper = context.spawn(Scraper(exporter, scrapeRule), s"scraper-${getCrawlerName(url)}")
         context.watchWith(scraper, ChildTerminated())
         scraper ! ScraperCommands.Scrape(ScrapeDocument(document.content, document.url))
 
+      /**
+       * Internal function to handle the communication with the Coordinator to check the valid links to visit.
+       * @param document document obtained by fetching the URL
+       */
       def checkPages(document: CrawlDocument): Unit =
         this.coordinator ! CoordinatorCommand.CheckPages(explorationPolicy(document).map(_.toString).toList, context.self)
 
@@ -127,6 +169,11 @@ class Crawler[D <: Document, T](context: ActorContext[CrawlerCommand],
             context.log.info(s"${context.self.path.name} has reached max depth! Terminating...")
             Behaviors.stopped
 
+    /**
+     * Sub-behavior for the Crawler that recursively spawns the children Crawlers.
+     * @param links valid links that get visited. One [[Crawler]] is spawned for each.
+     * @return the behavior
+     */
     def visitChildren(links: Iterator[String]): Behavior[CrawlerCommand] =
       val linkList = links.toList
       for
@@ -148,6 +195,12 @@ class Crawler[D <: Document, T](context: ActorContext[CrawlerCommand],
         buffer.stash(x)
         Behaviors.same
 
+  /**
+   * Last behavior for the Crawler. During this phase, the Crawler waits for all its children (crawlers and scraper) to
+   * have finished its job. If all the children have terminated, it ends its execution.
+   * @param alive number of children to wait
+   * @return the waiting behavior
+   */
   private def waitingForChildren(alive: Int): Behavior[CrawlerCommand] =
     context.log.info(s"${context.self.path.name} -> Children alive: $alive")
     if alive == 0 then
