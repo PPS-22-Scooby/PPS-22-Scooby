67370568d8e5377b51468247edeba31f781a1e14
Giovanni Antonioni <giovanni.antonioni2@studio.unibo.it>
2024-07-19 22:50:05 +0200

    feature: PPS-65 edit ExportRouter type

--- src/main/scala/core/exporter/ExporterRouter.scala
+++ src/main/scala/core/exporter/ExporterRouter.scala
@@ -2,19 +2,8 @@ package org.unibo.scooby
 package core.exporter
 
 import akka.actor.typed.ActorRef
-import akka.actor.typed.receptionist.{Receptionist, ServiceKey}
-import akka.actor.typed.scaladsl.{Behaviors, Routers}
+import akka.routing.BroadcastGroup
 
 object ExporterRouter:
-  def apply(exporters: Set[ActorRef[ExporterCommands]], routingKey: String, routerName: String) = Behaviors.setup[Unit]:
-    context =>
-      val serviceKey: ServiceKey[ExporterCommands.Export[?]] = ServiceKey(routingKey)
-
-      exporters foreach(context.system.receptionist ! Receptionist.Register(serviceKey, _))
-
-      val group = Routers.group(serviceKey)
-      val router = context.spawn(group, routerName)
-
-    Behaviors.empty
-
-
+  def apply(exporters: Seq[ActorRef[ExporterCommands]], routerName: String = "exp-router"): BroadcastGroup =
+    BroadcastGroup(exporters.map(_.toString), routerName)
