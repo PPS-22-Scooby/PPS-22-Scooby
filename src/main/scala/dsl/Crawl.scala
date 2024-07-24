3edd58fa64d61ffdd9bc851d789e881cb9c65cd2
FreshMag <pescomagnani@gmail.com>
2024-07-24 11:54:24 +0200

    feature: PPS-78 made Crawl DSL keywords safe

--- src/main/scala/dsl/Crawl.scala
+++ src/main/scala/dsl/Crawl.scala
@@ -7,26 +7,41 @@ import utility.document.CrawlDocument
 import utility.http.URL
 
 import monocle.syntax.all.*
+import _root_.dsl.syntax.catchRecursiveCtx
 
 object Crawl:
+  export SafeOps.*
+
   case class CrawlContext(var url: String, var policy: ExplorationPolicy)
 
   case class CrawlDocumentContext(var provider: ExplorationPolicy)
 
-  def crawl[T](init: CrawlContext ?=> Unit)(using context: ConfigurationBuilder[T]): Unit =
-    given builder: CrawlContext = CrawlContext("", context.configuration.crawlerConfiguration.explorationPolicy)
-    init
-    context.configuration = context.configuration
-      .focus(_.crawlerConfiguration.url)                 .replace(URL(builder.url))
-      .focus(_.crawlerConfiguration.explorationPolicy)   .replace(builder.policy)
+  object SafeOps:
+    import UnsafeOps.* 
 
-  def url(init: => String)(using builder: CrawlContext): Unit =
-    builder.url = init
+    inline def crawl[T](init: CrawlContext ?=> Unit)(using context: ConfigurationBuilder[T]): Unit =
+      catchRecursiveCtx[CrawlContext]("crawl")
+      crawlOp(init)
 
-  def policy(init: CrawlDocument ?=> Iterable[URL])(using builder: CrawlContext): Unit =
+    inline def policy(init: CrawlDocument ?=> Iterable[URL])(using builder: CrawlContext): Unit =
+      catchRecursiveCtx[CrawlDocument]("policy")
+      policyOp(init)
+
+  private[Crawl] object UnsafeOps:
+    def crawlOp[T](init: CrawlContext ?=> Unit)(using context: ConfigurationBuilder[T]): Unit =
+      given builder: CrawlContext = CrawlContext("", context.configuration.crawlerConfiguration.explorationPolicy)
+      init
+      context.configuration = context.configuration
+        .focus(_.crawlerConfiguration.url)                 .replace(URL(builder.url))
+        .focus(_.crawlerConfiguration.explorationPolicy)   .replace(builder.policy)
+
+    def policyOp(init: CrawlDocument ?=> Iterable[URL])(using builder: CrawlContext): Unit =
     builder.policy = doc =>
       given CrawlDocument = doc
       init
+
+  def url(init: => String)(using builder: CrawlContext): Unit =
+    builder.url = init
 
   def hyperlinks(using crawlDocumentContext: CrawlDocument): Iterable[URL] = 
     crawlDocumentContext.frontier
