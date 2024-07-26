4554341cb9ccf857a5527bb3c575cc38984f57b0
Valerio Di Zio <diziovalerio@gmail.com>
2024-07-26 11:47:47 +0200

    docs: PPS-75 add docs for crawl in dsl

--- src/main/scala/dsl/Crawl.scala
+++ src/main/scala/dsl/Crawl.scala
@@ -12,47 +12,116 @@ import dsl.syntax.catchRecursiveCtx
 object Crawl:
   export SafeOps.*
 
+  /**
+   * Context under "crawl"
+   *
+   * @param url    the URL to start crawling from
+   * @param policy the exploration policy to use
+   */
   case class CrawlContext(var url: String, var policy: ExplorationPolicy)
 
+  /**
+   * Type alias representing the section under "crawl"
+   */
   private type CrawlScope = CrawlContext ?=> Unit
+  /**
+   * Type alias representing the section under "policy"
+   */
   private type PolicyScope = CrawlDocument ?=> Iterable[URL]
 
   object SafeOps:
-    import UnsafeOps.* 
+    import UnsafeOps.*
 
-    inline def crawl[T](block: CrawlScope)(using context: ConfigurationBuilder[T]): Unit =
+    /**
+     * Top level keyword for defining the crawl configuration
+     *
+     * @param block   nested definitions of crawl settings
+     * @param globalScope global scooby scope
+     * @tparam T type of the configuration
+     */
+    inline def crawl[T](block: CrawlScope)(using globalScope: ConfigurationBuilder[T]): Unit =
       catchRecursiveCtx[CrawlContext]("crawl")
       crawlOp(block)
 
-    inline def policy(blocK: PolicyScope)(using builder: CrawlContext): Unit =
+    /**
+     * "crawl" level keyword for setting the exploration policy
+     *
+     * @param block   nested definitions of crawl settings
+     * @param builder context provided by the "crawl" keyword
+     */
+    inline def policy(block: PolicyScope)(using builder: CrawlContext): Unit =
       catchRecursiveCtx[CrawlDocument]("policy")
-      policyOp(blocK)
+      policyOp(block)
 
     extension (x: Iterable[URL])
+      /**
+       * Extension method to filter out URLs based on a predicate
+       *
+       * @param pred the predicate to filter URLs
+       * @return a collection of URLs that do not match the predicate
+       */
       infix def not(pred: URL => Boolean): Iterable[URL] =
         x.filterNot(pred)
 
   private[Crawl] object UnsafeOps:
-    def crawlOp[T](block: CrawlScope)(using context: ConfigurationBuilder[T]): Unit =
-      given builder: CrawlContext = CrawlContext("", context.configuration.crawlerConfiguration.explorationPolicy)
+
+    /**
+     * Internal operation to handle the crawl configuration
+     *
+     * @param block   nested definitions of crawl settings
+     * @param globalScope global scooby scope
+     * @tparam T type of the configuration
+     */
+    def crawlOp[T](block: CrawlScope)(using globalScope: ConfigurationBuilder[T]): Unit =
+      given builder: CrawlContext = CrawlContext("", globalScope.configuration.crawlerConfiguration.explorationPolicy)
       block
-      context.configuration = context.configuration
+      globalScope.configuration = globalScope.configuration
         .focus(_.crawlerConfiguration.url)                 .replace(URL(builder.url))
         .focus(_.crawlerConfiguration.explorationPolicy)   .replace(builder.policy)
 
-    def policyOp(block: PolicyScope)(using builder: CrawlContext): Unit =
+    /**
+     * Internal operation to handle the exploration policy
+     *
+     * @param policy  the exploration policy to use
+     * @param builder context provided by the "crawl" keyword
+     */
+    def policyOp(policy: PolicyScope)(using builder: CrawlContext): Unit =
     builder.policy = doc =>
       given CrawlDocument = doc
-      block
+      policy
 
+  /**
+   * "crawl" level keyword for setting the URL to start crawling from
+   *
+   * @param init    the URL to start crawling from
+   * @param builder context provided by the "crawl" keyword
+   */
   def url(init: => String)(using builder: CrawlContext): Unit =
     builder.url = init
 
+  /**
+   * Retrieves all hyperlinks, present in the body, from the current crawl document context
+   *
+   * @param crawlDocumentContext context provided by the "policy" keyword
+   * @return a collection of URLs representing hyperlinks
+   */
   def hyperlinks(using crawlDocumentContext: CrawlDocument): Iterable[URL] = 
     crawlDocumentContext.frontier
 
+  /**
+   * Retrieves all links, in the entire html file (including head tag), from the current crawl document context
+   *
+   * @param crawlDocumentContext context provided by the "policy" keyword
+   * @return a collection of URLs representing all link occurrences
+   */
   def allLinks(using crawlDocumentContext: CrawlDocument): Iterable[URL] = 
     crawlDocumentContext.getAllLinkOccurrences
 
+  /**
+   * Predicate to check if a URL is external to the current document's domain
+   *
+   * @param document the current crawl document context
+   * @return a function that checks if a URL is external
+   */
   def external(using document: CrawlDocument): URL => Boolean =
     _.domain != document.url.domain
