59469b9758ada959a3994ae5459d691bed4b297b
Giovanni Antonioni <giovanni.antonioni2@studio.unibo.it>
2024-07-26 10:48:36 +0200

    test: PPS-86 add test file for DSL scraper

---
+++ src/test/scala/dsl/DSLScraperTest.scala
@@ -0,0 +1,10 @@
+package org.unibo.scooby
+package dsl
+
+import org.scalatest.BeforeAndAfterEach
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
+
+class DSLScraperTest extends AnyFlatSpec, ScoobyEmbeddable, Matchers, BeforeAndAfterEach:
+  
+  
