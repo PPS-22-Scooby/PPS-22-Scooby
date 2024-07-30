49f6abd2e16d13ed5a8fa3e058722b2dbd10d3ea
Luca Rubboli <luca.rubboli2@studio.unibo.it>
2024-07-30 16:06:41 +0200

    test: PPS-107 provided test for scraper single usage

--- src/test/scala/core/scraper/ScraperTest.scala
+++ src/test/scala/core/scraper/ScraperTest.scala
@@ -1,18 +1,20 @@
 package org.unibo.scooby
 package core.scraper
 
+import akka.actor.DeadLetter
 import akka.actor.testkit.typed.scaladsl.{ActorTestKit, TestProbe}
 import akka.actor.typed.{ActorRef, ActorSystem}
-import org.scalatest.BeforeAndAfterAll
+import org.scalatest.{BeforeAndAfterAll, BeforeAndAfterEach}
 import org.scalatest.matchers.should.Matchers
 import utility.document.ScrapeDocument
 import utility.http.URL
-import core.scraper.ScraperPolicies.{given, _}
+import core.scraper.ScraperPolicies.{*, given}
 import org.scalatest.flatspec.AnyFlatSpec
-
 import core.exporter.ExporterCommands
 
-class ScraperTest extends AnyFlatSpec with Matchers with BeforeAndAfterAll:
+import scala.compiletime.uninitialized
+
+class ScraperTest extends AnyFlatSpec with Matchers with BeforeAndAfterAll with BeforeAndAfterEach:
 
   val classSelector: Seq[String] = Seq("testClass1", "testClass2")
   val idSelector: Seq[String] = Seq("testId1", "testId2")
@@ -57,16 +59,28 @@ class ScraperTest extends AnyFlatSpec with Matchers with BeforeAndAfterAll:
   val document: ScrapeDocument = ScrapeDocument(content, URL.empty)
 
   val testKit: ActorTestKit = ActorTestKit()
-  implicit val system: ActorSystem[Nothing] = testKit.system
+  implicit val system: ActorSystem[?] = testKit.system
   val exporterProbe: TestProbe[ExporterCommands] = testKit.createTestProbe[ExporterCommands]()
 
-  val scraperId: ActorRef[ScraperCommands] = testKit.spawn(Scraper(exporterProbe.ref, ScraperPolicies.scraperRule(idSelector, "id")))
-  val scraperTag: ActorRef[ScraperCommands] = testKit.spawn(Scraper(exporterProbe.ref, ScraperPolicies.scraperRule(tagSelector, "tag")))
-  val scraperClass: ActorRef[ScraperCommands] = testKit.spawn(Scraper(exporterProbe.ref, ScraperPolicies.scraperRule(classSelector, "class")))
-  val scraperCss: ActorRef[ScraperCommands] = testKit.spawn(Scraper(exporterProbe.ref, ScraperPolicies.scraperRule(cssSelector, "css")))
-  val scraperRegEx: ActorRef[ScraperCommands] = testKit.spawn(Scraper(exporterProbe.ref, ScraperPolicies.scraperRule(Seq(regEx), "regex")))
-  val scraperMultiPolicies: ActorRef[ScraperCommands] = testKit.spawn(Scraper(exporterProbe.ref, ScraperPolicies.scraperRule(idSelector, "id").concat(ScraperPolicies.scraperRule(Seq(regEx), "regex"))))
-  val scraperNoMatch: ActorRef[ScraperCommands] = testKit.spawn(Scraper(exporterProbe.ref, ScraperPolicies.scraperRule(idSelectorNoMatch, "id")))
+  var scraperId: ActorRef[ScraperCommands] = uninitialized
+  var scraperTag: ActorRef[ScraperCommands] = uninitialized
+  var scraperClass: ActorRef[ScraperCommands] = uninitialized
+  var scraperCss: ActorRef[ScraperCommands] = uninitialized
+  var scraperRegEx: ActorRef[ScraperCommands] = uninitialized
+  var scraperMultiPolicies: ActorRef[ScraperCommands] = uninitialized
+  var scraperNoMatch: ActorRef[ScraperCommands] = uninitialized
+
+  override def beforeEach(): Unit =
+    scraperId = testKit.spawn(Scraper(exporterProbe.ref, ScraperPolicies.scraperRule(idSelector, "id")))
+    scraperTag = testKit.spawn(Scraper(exporterProbe.ref, ScraperPolicies.scraperRule(tagSelector, "tag")))
+    scraperClass = testKit.spawn(Scraper(exporterProbe.ref, ScraperPolicies.scraperRule(classSelector, "class")))
+    scraperCss = testKit.spawn(Scraper(exporterProbe.ref, ScraperPolicies.scraperRule(cssSelector, "css")))
+    scraperRegEx = testKit.spawn(Scraper(exporterProbe.ref, ScraperPolicies.scraperRule(Seq(regEx), "regex")))
+    scraperMultiPolicies = testKit.spawn(Scraper(exporterProbe.ref, ScraperPolicies.scraperRule(idSelector, "id").concat(ScraperPolicies.scraperRule(Seq(regEx), "regex"))))
+    scraperNoMatch = testKit.spawn(Scraper(exporterProbe.ref, ScraperPolicies.scraperRule(idSelectorNoMatch, "id")))
+
+  override def afterAll(): Unit =
+    testKit.shutdownTestKit()
 
   "Scraper" should "process a document and send the result to exporter" in:
     val expectedById: Result[String] = idSelector.map(document.getElementById).map(_.fold(Result.empty[String])(el => Result.fromData(el.outerHtml)))
@@ -105,5 +119,18 @@ class ScraperTest extends AnyFlatSpec with Matchers with BeforeAndAfterAll:
     scraperNoMatch ! ScraperCommands.Scrape(document)
     exporterProbe.expectMessage(ExporterCommands.Export(Result.empty[String]))
 
-  override def afterAll(): Unit =
-    testKit.shutdownTestKit()
+  "Scraper" should "not be able to process more than one scrape message" in:
+
+    val deadLettersProbe = testKit.createTestProbe[DeadLetter]()
+    system.eventStream ! akka.actor.typed.eventstream.EventStream.Subscribe(deadLettersProbe.ref)
+
+    val scraperList = Seq(scraperId, scraperTag, scraperClass, scraperCss, scraperRegEx)
+
+    scraperList.foreach:
+      scraper =>
+        scraper ! ScraperCommands.Scrape(document)
+        deadLettersProbe.expectNoMessage()
+        scraper ! ScraperCommands.Scrape(document)
+        val deadLetter = deadLettersProbe.expectMessageType[DeadLetter]
+        deadLetter.message shouldBe ScraperCommands.Scrape(document)
+        deadLetter.recipient.path shouldBe scraper.path
